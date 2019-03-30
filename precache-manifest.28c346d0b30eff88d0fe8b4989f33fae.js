b7325cdedecfb7050408d3d56d70f238683a41b1
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2019-03-30 17:33:25 +0300

    Updates

---
+++ precache-manifest.28c346d0b30eff88d0fe8b4989f33fae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e4a2950e84cddd79aca3f26364fa3f",
+    "url": "/natbyggreact/static/media/rawpixel-678089-unsplash.a3e4a295.jpg"
+  },
+  {
+    "revision": "ae01282ee528eb363982ded20ba55d23",
+    "url": "/natbyggreact/static/media/logga_transparent_blue.ae01282e.png"
+  },
+  {
+    "revision": "374344147cff8606f835d752944d8570",
+    "url": "/natbyggreact/static/media/light-grey-terrazzo.37434414.png"
+  },
+  {
+    "revision": "8080e0cc0f0ae51b5291",
+    "url": "/natbyggreact/static/js/runtime~main.da2cf38f.js"
+  },
+  {
+    "revision": "ae8c25e20f32847c4769",
+    "url": "/natbyggreact/static/js/main.e22a653e.chunk.js"
+  },
+  {
+    "revision": "00fcd75589ee1171e7fe",
+    "url": "/natbyggreact/static/js/2.4e7e7926.chunk.js"
+  },
+  {
+    "revision": "ae8c25e20f32847c4769",
+    "url": "/natbyggreact/static/css/main.4447e244.chunk.css"
+  },
+  {
+    "revision": "2cf568cbebebfe3fe3327cd5acb5d1ad",
+    "url": "/natbyggreact/index.html"
+  }
+];
